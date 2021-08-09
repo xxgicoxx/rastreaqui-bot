8cac464e517f4607c8e24c9ac4553a3eef81c552
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2021-08-09 01:35:51 -0300

    Update dependencies

--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,8 @@ fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
