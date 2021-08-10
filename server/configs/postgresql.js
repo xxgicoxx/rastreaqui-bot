a28f9ae1c73280c8a457aaff30fc8369cdea4653
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2021-08-10 00:49:37 -0300

    Fix POSTGRESQL_DATABASE

--- server/configs/postgresql.js
+++ server/configs/postgresql.js
@@ -1,7 +1,7 @@
 const postgresql = {
   username: process.env.POSTGRESQL_USERNAME || 'rastreaqui',
   password: process.env.POSTGRESQL_PASSWORD || '123456',
-  database: process.env.POSTGRESQL_NAME || 'rastreaqui',
+  database: process.env.POSTGRESQL_DATABASE || 'rastreaqui',
   host: process.env.POSTGRESQL_HOST || '127.0.0.1',
   dialect: process.env.POSTGRESQL_DIALECT || 'postgres',
 };
